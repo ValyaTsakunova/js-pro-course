7210ff981bd4d868d4d5b9c61e2cf1efaa542928
Valya Tsakunova <vtsakunova@mail.ru>
2021-04-07 22:18:38 +0300

    make fixes

--- react-counter/src/components/Counter.jsx
+++ react-counter/src/components/Counter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
 
-const element = <FontAwesomeIcon icon={faSync} />;
+const dischargeButtonIcon = <FontAwesomeIcon icon={faSync} />;
 
 class Counter extends React.Component{
     constructor(props){
@@ -14,15 +14,15 @@ class Counter extends React.Component{
     }
 
     plus = () => {
-        this.setState({
-            value: this.state.value + 1,
-        })
+        this.setState((prevState) => ({
+            value: prevState.value + 1,
+        }))
     }
 
     minus = () => {
-        this.setState({
-            value: this.state.value - 1,
-        })
+        this.setState((prevState) => ({
+            value: prevState.value - 1,
+        }))
     }
 
     discharge = () => {
@@ -38,7 +38,7 @@ class Counter extends React.Component{
                     {this.state.value}
                 </div>
                 <button className="plusButton" onClick={this.plus}></button>
-                <button className="dischargeButton" onClick={this.discharge}>{element}</button>
+                <button className="dischargeButton" onClick={this.discharge}>{dischargeButtonIcon}</button>
                 <button className="minusButton" onClick={this.minus}></button>
             </div>
         )
